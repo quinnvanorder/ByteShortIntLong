eec8f366c89e286e87ca730b66c06305b292bf47
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-09 19:43:35 -0700

    Byte, Short, Long and the bit width that stores them

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int myValue = 10000;
-
+        //32 bits
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
@@ -14,6 +14,34 @@ public class Main {
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2147483647;
+
+        // 8 bits
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        //16 bits
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        //64 bits
+        long myLongValue = 100L;
+        //intellij suggests capitol L when defining long to avoid confusion with lower case looking like number
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2147483647234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+
 
     }
 }
